ae1d43895d1273999bd4ef9336d9a62030d4e0c2
APIVerve Team <hello@apiverve.com>
2025-03-28 18:09:11 -0500

    Bug Fixes

---
+++ src/apiverve_workingdays/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_workingdays',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Working Days is a simple tool for getting the number of working days in a month. It returns the number of working days in the given month.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
